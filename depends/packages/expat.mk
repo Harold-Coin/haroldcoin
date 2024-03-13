e41030115b7d5510df69a6224855ea4031997a07
Setvin <117243445+realsetvin@users.noreply.github.com>
2024-03-13 19:45:34 -0400

    Update expat.mk

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,5 +1,5 @@
 package=expat
-$(package)_version=2.1.0
+$(package)_version=2.3.0
 $(package)_download_path=http://sourceforge.net/projects/expat/files/expat/$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=823705472f816df21c8f6aa026dd162b280806838bb55b3432b0fb1fcca7eb86
