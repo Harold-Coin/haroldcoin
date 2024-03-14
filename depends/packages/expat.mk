db46752c4835efe907e5088424865bd9406e714f
Setvin <117243445+realsetvin@users.noreply.github.com>
2024-03-13 20:05:46 -0400

    Update expat.mk

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.4.1
+$(package)_version=2.6.2
 $(package)_download_path=http://sourceforge.net/projects/expat/files/expat/$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=823705472f816df21c8f6aa026dd162b280806838bb55b3432b0fb1fcca7eb86
+$(package)_sha256_hash=d4cf38d26e21a56654ffe4acd9cd5481164619626802328506a2869afab29ab3
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static
